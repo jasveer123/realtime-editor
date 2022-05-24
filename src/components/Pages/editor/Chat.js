839106bfa1f2b62608e7aaf9fcfd9dec407e0922
jasveer123 <jasveer3101998@gmail.com>
2022-05-24 12:25:05 +0530

    chat update

--- src/components/Pages/editor/Chat.js
+++ src/components/Pages/editor/Chat.js
@@ -1,18 +1,39 @@
 import React from 'react'
 import './chat.css'
+import Avatar from 'react-avatar'
+import { BsChatDots } from 'react-icons/bs'
+import ScrollableFeed from 'react-scrollable-feed'
 
 function Chat(props) {
   return (
     <>
+      <div className="heading">
+        Live chat
+        <BsChatDots className="bs" />
+      </div>
       {props.in.map((payload, index) => {
         return (
           <div
-            className="talk-bubble tri-right left-top"
-            key={index}
-            id={props.username === payload.username ? 'you' : 'other'}
+            className="wrapperdivid"
+            id={
+              props.username === payload.username
+                ? 'wrapperyou'
+                : 'wrapperother'
+            }
           >
-            <div className="talktext">
-              <p>{payload.message}</p>
+            <div
+              className="talk-bubble tri-right left-top"
+              key={index}
+              id={props.username === payload.username ? 'you' : 'other'}
+            >
+              <div className="talktext">
+                <p>{payload.message}</p>
+              </div>
+            </div>
+            <div
+              id={props.username === payload.username ? 'youdiv' : 'otherdiv'}
+            >
+              <Avatar name={payload.username} size="40px" round="25px" />
             </div>
           </div>
         )
